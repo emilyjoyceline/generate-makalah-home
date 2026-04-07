3b1ec6006ca899c1544851bea3bb30c96e8c1dcc
Emily Joyceline <119916119+emilyjoyceline@users.noreply.github.com>
2026-04-07 13:24:46 +0700

    Add closing box with message in script.js
    temporary closing box with message

--- script.js
+++ script.js
@@ -411,7 +411,9 @@ function generateHTMLForPDF(data, logoBase64) {
                 ${data.penutup ? `
                     <h3 style="font-size: 20px; margin-top: 30px; color: #003d82;">Penutup:</h3>
                     <p style="text-align: justify;">${data.penutup}</p>
-                    
+                    <div class="closing-box">
+                        Kiranya makalah HOME ini menolong kita bukan hanya memahami Yesus,<br>tetapi mengikuti jejak hidup-Nya. Tuhan Memberkati.
+                    </div>
                 ` : ''}
             </div>
         </body>
